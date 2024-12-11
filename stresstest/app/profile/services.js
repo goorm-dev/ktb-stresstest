9262e99fca90d019fe809990443ae2034502f114
Jungmin Lee <starjungmin@gmail.com>
2024-12-11 10:17:43 +0900

    docs: 부하 테스트 결과 분석 및 설정 최적화

--- stresstest/app/profile/services.js
+++ stresstest/app/profile/services.js
@@ -6,14 +6,14 @@ const addProfileImage = async (page, filename) => {
   await page.locator('//*[@id="__next"]/div/nav/div/div/div[3]/div/div/button[1]').click();
   console.log(page.url());
   const [fileChooser] = await Promise.all([
-      page.waitForEvent('filechooser'),
-      page.click('//*[@id="radix-:rj:"]/div/div/div/div[2]/button'),
+    page.waitForEvent('filechooser'),
+    page.click('//*[@id="radix-:rj:"]/div/div/div/div[2]/button'),
   ]);
-  
+
   await fileChooser.setFiles(path.resolve(filename));
   await page.getByRole('button', { name: '저장' }).click();
   await page.waitForTimeout(3000);
-  
+
   console.info('Profile image added');
 };
 
