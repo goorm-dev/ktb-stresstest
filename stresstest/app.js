dd908efa397581e74f681b2607fd5f29e4e83d17
Jungmin Lee <starjungmin@gmail.com>
2024-12-11 20:02:04 +0900

    chore: update Playwright dependency and modify stress test configuration
    - Added Playwright as a dependency in package.json and package-lock.json.
    - Updated the target URL in playwright-artillery.yml and stresstest/app.js to a new IP address.
    - Refactored stress test scenarios to simplify the load testing configuration.
    - Changed the test function name in the Playwright scenario from "createNewChat" to "generateChatAiResponse".
    - Enhanced error handling and visibility checks in chat functions for improved reliability.

--- stresstest/app.js
+++ stresstest/app.js
@@ -8,7 +8,7 @@ const crypto = require('crypto');
 const passwd = "123123";
 const domain = "@test.com";
 const chatName = "asdfasdf";
-const site = "http://3.35.19.209:3000/";
+const site = "http://13.124.37.26:3000/";
 const filename = './photo/test.jpeg';
 const aiMention = "@wayneAI";
 const findText = "hello";
@@ -16,11 +16,11 @@ const msg = "hello";
 const group = "group_b";
 const TEST_ACCOUNT = {
   email: 'test@test.com',
-  password: '123123'
+  password: 'test1234'
 };
 
-async function registerUser(page) {
-  const id = `${group}_${Date.now()}`
+async function registerUser(page, chatname) {
+  const id = chatname
   const email = id + domain;
 
   try {
@@ -46,51 +46,56 @@ async function loginTestUser(page) {
 
 async function loginUser(page) {
   //await loginTestUser(page);
-  await registerUser(page);
+  await loginTestUser(page);
 };
 
 async function createNewChat(page) {
   //await loginTestUser(page);
-  await registerUser(page);
+  await loginTestUser(page);
   await createChat(page, `${group}_${Date.now()}`);
 };
 
 async function scrollChat(page) {
   //await loginTestUser(page);
-  await registerUser(page);
+  await loginTestUser(page);
   await scrollDown(page);
 };
 
 async function sendMessageToChat(page) {
   //await loginTestUser(page);
-  await registerUser(page);
+  await loginTestUser(page);
+
+  //const chatName = `sendMessageToChat_${Date.now()}`; // 동일한 chatName 사용
+  // await registerUser(page);
+  //await createChat(page, chatName);
   await accessChat(page, chatName);
   await talkChat(page, msg);
-};
+}
 
 async function reactionToMessage(page) {
   //await loginTestUser(page);
-  await registerUser(page);
+  await loginTestUser(page);
   await accessChat(page, chatName);
   await addReactions(page, findText);
 };
 
 async function uploadFileToChat(page) {
   //await loginTestUser(page);
-  await registerUser(page);
+  await loginTestUser(page);
   await accessChat(page, chatName);
   await uploadFile(page, filename);
 };
 
 async function updateProfileImage(page) {
   //await loginTestUser(page);
-  await registerUser(page);
-  await addProfileImage(page, filename);
+  await loginTestUser(page);
+  //await accessChat(page, chatName);
+  await addProfileImage(page, filename, site);
 };
 
 async function generateChatAiResponse(page) {
   //await loginTestUser(page);
-  await registerUser(page);
+  await loginTestUser(page);
   await accessChat(page, chatName);
   await generateAiResponse(page, aiMention);
 };
--- stresstest/app/profile/services.js
+++ stresstest/app/profile/services.js
@@ -1,15 +1,23 @@
 const { expect } = require('@playwright/test');
 const path = require('path');
 
-const addProfileImage = async (page, filename) => {
-  await page.locator('//*[@id="__next"]/div/nav/div/div/div[3]/div/button').click();
-  await page.locator('//*[@id="__next"]/div/nav/div/div/div[3]/div/div/button[1]').click();
+const addProfileImage = async (page, filename, site) => {
+
+
+  await page.goto(`${site}/profile`); //site 안쓰고 버튼눌러서 이동하기...?
+  console.info('Navigated to profile page');
+
   console.log(page.url());
+  //const changeImageButton = page.locator('button._3PIsE.vapor-Button-md.vapor-Button-secondary_fill.rounded-full.p-2.mt-3');
+  //await changeImageButton.click();
+  //*[@id="radix-:r1:"]/div/div/div/div[2]/button[1]
   const [fileChooser] = await Promise.all([
     page.waitForEvent('filechooser'),
-    page.click('//*[@id="radix-:rj:"]/div/div/div/div[2]/button'),
+    page.locator('xpath=/html/body/div/div/main/div/article/div/div/div/div/div[2]/button[1]').click(), //초기 경우에도 프로필 변경할 수 있게 변경하기
   ]);
-
+  // //*[@id="__next"]/div/nav/div/div/div[2]/div/button[1]
+  // /html/body/div/div/nav/div/div/div[2]/div/button
+  //#radix-\:r1\: > div > div > div > div.mt-6 > button._3PIsE.vapor-Button-md.vapor-Button-secondary_fill.rounded-full.p-2.mt-3
   await fileChooser.setFiles(path.resolve(filename));
   await page.getByRole('button', { name: '저장' }).click();
   await page.waitForTimeout(3000);
